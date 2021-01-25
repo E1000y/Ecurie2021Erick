6d78a670a9b85851c3a20a3447002d7786af2089
Emilie <emilie.paniagua@gmail.com>
2021-01-25 13:20:15 +0100

    Added JTable

---
+++ src/main/java/com/ui/Table.java
@@ -0,0 +1,18 @@
+package com.ui;
+
+import javax.swing.*;
+
+public class Table extends JFrame {
+
+    JTable jTable;
+
+
+    public Table(){
+        setSize(400,400);
+        
+
+
+
+        setVisible(true);
+    }
+}
