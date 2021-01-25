273865b0c8c631ecf2e0e959b3a7337253d2a642
Emilie <emilie.paniagua@gmail.com>
2021-01-25 23:07:03 +0100

    before refactorisation

--- src/main/java/com/ui/Table.java
+++ src/main/java/com/ui/Table.java
@@ -1,18 +1,23 @@
 package com.ui;
 
-import com.domain.Horse;
-import com.domain.Rider;
+
 
 import javax.swing.*;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
-import java.util.Arrays;
-import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Table extends JFrame {
+
+public class Table extends JFrame implements ActionListener {
 
     JTable jTable;
     HorseTableModel horseTableModel;
+    JTextField filterText;
+    TableRowSorter<HorseTableModel> sorter;
+    JButton button;
+    JPanel panel;
+    JLabel label;
 
     public Table(){
         setSize(400,400);
@@ -20,13 +25,27 @@ public class Table extends JFrame {
 
         horseTableModel = new HorseTableModel();
         jTable = new JTable(horseTableModel);
+        sorter = new TableRowSorter<>(horseTableModel);
+        jTable.setRowSorter(sorter);
+        filterText = new JTextField("");
 
-        jTable.setAutoCreateRowSorter(true);
+
         Container c = getContentPane();
 
+
+
+        panel = new JPanel(new BorderLayout());
+        label = new JLabel("Filter");
+        panel.add(label, BorderLayout.WEST);
+        filterText = new JTextField("");
+        panel.add(filterText, BorderLayout.CENTER);
+        button = new JButton("Filter");
+
         c.add((new JScrollPane(jTable)));
+        c.add(button,BorderLayout.SOUTH);
+        c.add(panel, BorderLayout.NORTH);
 
-
+        button.addActionListener(this);
 
 
         setVisible(true);
@@ -37,5 +56,16 @@ Table table = new Table();
     }
 
 
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("filter clicked");
+        String text = filterText.getText();
+        System.out.println("text = " + text);
+        if (text.isBlank()) {
+            sorter.setRowFilter(null);
+        } else {
+            System.out.println("filter with text : "+text);
+            sorter.setRowFilter(RowFilter.regexFilter(text));
+        }
+    }
 }
